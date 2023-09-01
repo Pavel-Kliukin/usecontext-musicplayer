26cbcaa7f6e649c02d1079376e2f00c650e4391f
Pavel-Kliukin <9213087@mail.ru>
2023-09-01 10:41:21 +0300

    audioplayer changed to audioPlayer and one missing line in toggle added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
+import { PlayerControls } from './components/PlayerControls';
+import { TrackList } from './components/TrackList';
+import { MusicContext } from './contexts/MusicContext';
+import {useState} from "react"
+import music1 from './assets/Music1.mp3'
+import music2 from './assets/Music2.mp3'
 
 function App() {
+
+  const [state, setState] = useState ({
+    audioPlayer: new Audio(music1), 
+    tracks: [
+      {
+        name: "Track 1",
+        file: music1,
+      },
+      {
+        name: "Track 2",
+        file: music2,
+      }
+    ],
+    currentTrackIndex: 0,
+    isPlaying: false,
+  })
+
+  console.log("State: ", state);
+
+
   return (
-    <div className="App">
-   
-    </div>
+    <MusicContext.Provider value={[state, setState]}>
+      <div className="App">
+        <PlayerControls />
+        <TrackList />
+      </div>
+    </MusicContext.Provider>
   );
 }
 
